0c097b4ea070c070cc9f45e146e9bf68c152aef3
Zakaria DRISSI EL MALIANI <maliani.zakaria@gmail.com>
2020-12-28 23:46:13 +0100

    get data from cloud function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './service/auth.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient , HttpHeaders} from '@angular/common/http';
 
 
 @Component({
@@ -8,21 +8,29 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'inpt-cloud-mamgas';
   email: string;
   password: string;
-  data = '';
+  data: any = {};
 
   constructor(public authService: AuthService, private _httpClient: HttpClient) {
     this.email = '';
     this.password = '';
+
+    
+  }
+
+  ngOnInit() {
     this.getData();
   }
 
   getData(){
-    return this._httpClient.get("https://us-central1-cours-tp.cloudfunctions.net/helloWorld").subscribe(dataFirestore => {
+    console.log("before call API");
+    
+    return this._httpClient.get("http://us-central1-cours-tp.cloudfunctions.net/helloWorld/").subscribe(dataFirestore => {
       console.log("data", dataFirestore);
+      this.data = dataFirestore;
     });
   }
 
