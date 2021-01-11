13cec994dce2548da8686c5c32a10a3011cb1cb4
Zakaria DRISSI EL MALIANI <maliani.zakaria@gmail.com>
2021-01-11 14:01:04 +0100

    TP 3 : OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './service/auth.service';
-import { HttpClient , HttpHeaders} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
