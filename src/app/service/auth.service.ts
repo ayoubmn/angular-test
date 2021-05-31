8460f419f2e2fb955a1822458a12beaa73f61f33
Hyperledger <ayoubmn.1999n1@gmail.com>
2021-05-31 19:06:45 +0100

    google authentication

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
-
+import firebase from 'firebase/app';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,9 +9,26 @@ export class AuthService {
 
   user: Observable<any>;
 
-  constructor(private firebaseAuth: AngularFireAuth) {
+  constructor(public firebaseAuth: AngularFireAuth) {
     this.user = firebaseAuth.authState;
   }
+
+  // Sign in with Google
+  GoogleAuth() {
+    return this.AuthLogin(new firebase.auth
+      .GoogleAuthProvider());
+  }  
+
+  // Auth logic to run auth providers
+  AuthLogin(provider: firebase.auth.AuthProvider) {
+    return this.firebaseAuth.signInWithPopup(provider)
+    .then((result) => {
+        console.log('You have been successfully logged in!')
+    }).catch((error) => {
+        console.log(error)
+    })
+  }
+
 
   signup(email: string, password: string) {
     this.firebaseAuth
